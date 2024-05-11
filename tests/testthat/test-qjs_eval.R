68d866007d0773dd3f8449cf521c1b47e5a320a1
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-05-12 01:10:55 +0300

    Replace testthat with tinytest

--- tests/testthat/test-qjs_eval.R
+++
@@ -1,8 +0,0 @@
-test_that("qjs_eval returns correct type & value", {
-  expect_equal(1, qjs_eval("1"))
-  expect_equal(2.5, qjs_eval("1 + 1.5"))
-  expect_equal("Hello World!", qjs_eval("'Hello World!'"))
-  expect_equal("Hello World!", qjs_eval("'Hello' + ' ' + 'World!'"))
-  expect_equal(list(a = 1, b = "2"),
-                qjs_eval("var t = {'a' : 1, 'b' : '2'}; t"))
-})
