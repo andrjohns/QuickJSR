18f1485f4421aef5f5a1f7f83e6a914224482437
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-05-15 16:43:16 +0300

    JSON test 2

--- inst/include/quickjsr/JSON_to_JSValue.hpp
+++ inst/include/quickjsr/JSON_to_JSValue.hpp
@@ -1,27 +1,21 @@
 #ifndef QUICKJSR_JSON_TO_JSVALUE_HPP
 #define QUICKJSR_JSON_TO_JSVALUE_HPP
 
-#include <cpp11.hpp>
-#include <quickjs-libc.h>
+#include <quickjsr/wrapper_classes.hpp>
 
 namespace quickjsr {
 
-JSValue JSON_to_JSValue(JSContext* ctx, const std::string& json) {
-  JSValue global = JS_GetGlobalObject(ctx);
-  JSValue json_obj = JS_GetPropertyStr(ctx, global, "JSON");
-  JSValue parse = JS_GetPropertyStr(ctx, json_obj, "parse");
+JSValueWrapper JSON_to_JSValue(JSContext* ctx, const std::string& json) {
+  JSValueWrapper global = JS_GetGlobalObject(ctx);
+  JSValueWrapper json_obj = JS_GetPropertyStr(ctx, global, "JSON");
+  JSValueWrapper parse = JS_GetPropertyStr(ctx, json_obj, "parse");
 
-  JSValue json_str = JS_NewString(ctx, json.c_str());
-  JSValue result = JS_Call(ctx, parse, global, 1, &json_str);
+  JSValueWrapper json_str = JS_NewString(ctx, json.c_str());
+  JSValueWrapper result = JS_Call(ctx, parse, global, 1, &json_str);
 
   if (JS_IsException(result)) {
     js_std_dump_error(ctx);
   }
-
-  JS_FreeValue(ctx, json_str);
-  JS_FreeValue(ctx, parse);
-  JS_FreeValue(ctx, json_obj);
-  JS_FreeValue(ctx, global);
 
   return result;
 }
