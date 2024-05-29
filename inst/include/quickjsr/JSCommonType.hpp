df7151a2ba42d1c7042cc48c7b7bf7fc5dc42c19
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-05-30 02:26:25 +0300

    Support passing objects and arrays from JS to R (#35)
    * Support passing objects from JS to R
    
    * Tidy

--- inst/include/quickjsr/JSCommonType.hpp
+++ inst/include/quickjsr/JSCommonType.hpp
@@ -10,10 +10,14 @@ enum JSCommonType {
   Double,
   Logical,
   Character,
-  Object
+  NumberArray,
+  Object,
+  Unknown
 };
 
-JSCommonType JS_GetCommonType(const JSValue& val) {
+JSCommonType JS_ArrayCommonType(JSContext* ctx, const JSValue& val);
+
+JSCommonType JS_GetCommonType(JSContext* ctx, const JSValue& val) {
   if (JS_IsBool(val)) {
     return Logical;
   }
@@ -26,18 +30,35 @@ JSCommonType JS_GetCommonType(const JSValue& val) {
   if (JS_IsString(val)) {
     return Character;
   }
-  return Object;
-}
-
-JSCommonType JS_UpdateCommonType(JSCommonType current, const JSValue& val) {
-  if (current == Object) {
+  if (JS_IsArray(ctx, val)) {
+    JSCommonType common_type = JS_ArrayCommonType(ctx, val);
+    if (common_type == Integer || common_type == Double) {
+      return NumberArray;
+    }
+  }
+  if (JS_IsObject(val)) {
     return Object;
   }
-  switch (JS_GetCommonType(val)) {
+  return Unknown;
+}
+
+JSCommonType JS_UpdateCommonType(JSCommonType current, JSContext* ctx, const JSValue& val) {
+  if (current == Object || current == Unknown) {
+    return current;
+  }
+
+  JSCommonType new_type = JS_GetCommonType(ctx, val);
+  if (current == new_type) {
+    return current;
+  }
+  // If both types are not NumberArray (checked above), return Object
+  if (new_type == NumberArray || current == NumberArray || new_type == Object) {
+    return Object;
+  }
+
+  switch (new_type) {
     case Integer: {
       switch (current) {
-        case Integer:
-          return Integer;
         case Double:
           return Double;
         case Logical:
@@ -45,43 +66,48 @@ JSCommonType JS_UpdateCommonType(JSCommonType current, const JSValue& val) {
         case Character:
           return Character;
         default:
-          return Object;
+          return Unknown;
       }
     }
     case Double: {
       switch (current) {
         case Integer:
           return Double;
-        case Double:
-          return Double;
         case Logical:
           return Double;
         case Character:
           return Character;
         default:
-          return Object;
+          return Unknown;
       }
     }
-    case Logical: {
-      switch (current) {
-        case Integer:
-          return Integer;
-        case Double:
-          return Double;
-        case Logical:
-          return Logical;
-        case Character:
-          return Character;
-        default:
-          return Object;
-      }
-    }
+    case Logical:
+      return current;
     case Character:
       return Character;
     default:
-      return Object;
+      return Unknown;
   }
 }
+
+JSCommonType JS_ArrayCommonType(JSContext* ctx, const JSValue& val) {
+  JSValue elem = JS_GetPropertyUint32(ctx, val, 0);
+  JSCommonType common_type = JS_GetCommonType(ctx, elem);
+  JS_FreeValue(ctx, elem);
+
+  uint32_t len;
+  JSValue arr_len = JS_GetPropertyStr(ctx, val, "length");
+  JS_ToUint32(ctx, &len, arr_len);
+  JS_FreeValue(ctx, arr_len);
+
+  for (uint32_t i = 1; i < len; i++) {
+    elem = JS_GetPropertyUint32(ctx, val, i);
+    common_type = JS_UpdateCommonType(common_type, ctx, elem);
+    JS_FreeValue(ctx, elem);
+  }
+  return common_type;
+}
+
 } // namespace quickjsr
 
 #endif
--- inst/include/quickjsr/JSValue_to_SEXP.hpp
+++ inst/include/quickjsr/JSValue_to_SEXP.hpp
@@ -7,12 +7,27 @@
 #include <quickjsr/JSCommonType.hpp>
 
 namespace quickjsr {
-// Forward declaration to allow for recursive calls
-SEXP JSValue_to_SEXP(JSContext* ctx, JSValue val);
 
-SEXP JSValue_to_SEXP_scalar(JSContext* ctx, JSValue val) {
+// Used internally in QuickJS, but not exposed in the public API, so redefine it here
+static void js_free_prop_enum(JSContext *ctx, JSPropertyEnum *tab, uint32_t len) {
+  uint32_t i;
+  if (tab) {
+    for (i = 0; i < len; i++) {
+      JS_FreeAtom(ctx, tab[i].atom);
+    }
+    js_free(ctx, tab);
+  }
+}
+
+// Forward declaration to allow for recursive calls
+SEXP JSValue_to_SEXP(JSContext* ctx, const JSValue& val);
+
+SEXP JSValue_to_SEXP_scalar(JSContext* ctx, const JSValue& val) {
   if (JS_IsBool(val)) {
     return cpp11::as_sexp(JSValue_to_Cpp<bool>(ctx, val));
+  }
+  if (JS_VALUE_GET_TAG(val) == JS_TAG_INT) {
+    return cpp11::as_sexp(JSValue_to_Cpp<int32_t>(ctx, val));
   }
   if (JS_IsNumber(val)) {
     return cpp11::as_sexp(JSValue_to_Cpp<double>(ctx, val));
@@ -23,25 +38,7 @@ SEXP JSValue_to_SEXP_scalar(JSContext* ctx, JSValue val) {
   return cpp11::as_sexp("Unsupported type");
 }
 
-JSCommonType JS_ArrayCommonType(JSContext* ctx, JSValue val) {
-  JSValue elem = JS_GetPropertyUint32(ctx, val, 0);
-  JSCommonType common_type = JS_GetCommonType(elem);
-  JS_FreeValue(ctx, elem);
-
-  uint32_t len;
-  JSValue arr_len = JS_GetPropertyStr(ctx, val, "length");
-  JS_ToUint32(ctx, &len, arr_len);
-  JS_FreeValue(ctx, arr_len);
-
-  for (uint32_t i = 1; i < len; i++) {
-    elem = JS_GetPropertyUint32(ctx, val, i);
-    common_type = JS_UpdateCommonType(common_type, elem);
-    JS_FreeValue(ctx, elem);
-  }
-  return common_type;
-}
-
-SEXP JSValue_to_SEXP_vector(JSContext* ctx, JSValue val) {
+SEXP JSValue_to_SEXP_vector(JSContext* ctx, const JSValue& val) {
   switch (JS_ArrayCommonType(ctx, val)) {
     case Integer:
       return cpp11::as_sexp(JSValue_to_Cpp<std::vector<int>>(ctx, val));
@@ -51,6 +48,28 @@ SEXP JSValue_to_SEXP_vector(JSContext* ctx, JSValue val) {
       return cpp11::as_sexp(JSValue_to_Cpp<std::vector<bool>>(ctx, val));
     case Character:
       return cpp11::as_sexp(JSValue_to_Cpp<std::vector<std::string>>(ctx, val));
+    case NumberArray: {
+      std::vector<std::vector<double>> res = JSValue_to_Cpp<std::vector<std::vector<double>>>(ctx, val);
+      // Check that the inner vectors are all the same length
+      size_t len = res[0].size();
+      bool allSameSize = std::all_of(res.begin() + 1, res.end(),
+                                    [len](auto&& vec) { return vec.size() == len; });
+      if (allSameSize) {
+        cpp11::writable::doubles_matrix<cpp11::by_column> out(res.size(), len);
+        for (size_t i = 0; i < res.size(); i++) {
+          for (size_t j = 0; j < len; j++) {
+            out(i, j) = res[i][j];
+          }
+        }
+        return out;
+      } else {
+        cpp11::writable::list out(res.size());
+        for (size_t i = 0; i < res.size(); i++) {
+          out[static_cast<R_xlen_t>(i)] = cpp11::as_sexp(res[i]);
+        }
+        return out;
+      }
+    }
     case Object: {
       uint32_t len;
       JSValue arr_len = JS_GetPropertyStr(ctx, val, "length");
@@ -70,11 +89,35 @@ SEXP JSValue_to_SEXP_vector(JSContext* ctx, JSValue val) {
   }
 }
 
-SEXP JSValue_to_SEXP(JSContext* ctx, JSValue val) {
+SEXP JSValue_to_SEXP_list(JSContext* ctx, const JSValue& val) {
+  // Get the keys of the object
+  JSPropertyEnum* tab = NULL;
+  uint32_t len = 0;
+  JS_GetOwnPropertyNames(ctx, &tab, &len, val, JS_GPN_STRING_MASK);
+  cpp11::writable::strings keys(len);
+  cpp11::writable::list out(len);
+  for (uint32_t i = 0; i < len; i++) {
+    JSValue elem = JS_GetProperty(ctx, val, tab[i].atom);
+    out[static_cast<R_xlen_t>(i)] = JSValue_to_SEXP(ctx, elem);
+
+    const char* key = JS_AtomToCString(ctx, tab[i].atom);
+    keys[static_cast<R_xlen_t>(i)] = key;
+
+    JS_FreeValue(ctx, elem);
+    JS_FreeCString(ctx, key);
+  }
+  js_free_prop_enum(ctx, tab, len);
+  out.attr("names") = keys;
+  return out;
+}
+
+SEXP JSValue_to_SEXP(JSContext* ctx, const JSValue& val) {
   if (JS_IsArray(ctx, val)) {
     return JSValue_to_SEXP_vector(ctx, val);
   }
-  // TODO: Implement array and object conversion
+  if (JS_IsObject(val)) {
+    return JSValue_to_SEXP_list(ctx, val);
+  }
   return JSValue_to_SEXP_scalar(ctx, val);
 }
 
