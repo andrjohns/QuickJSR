499ff7ec3816792925a9f5dc37736a3dc4cfb564
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-07-07 14:47:09 +0300

    Add additional jsvalue conversion helpers (#63)
    * Add additional jsvalue conversion helpers
    
    * valgrind
    
    * valgrind

--- inst/include/quickjsr/JSValue_to_Cpp.hpp
+++ inst/include/quickjsr/JSValue_to_Cpp.hpp
@@ -31,6 +31,20 @@ namespace quickjsr {
     return res;
   }
 
+  template <typename T, enable_if_type_t<uint32_t, T>* = nullptr>
+  T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
+    uint32_t res;
+    JS_ToUint32(ctx, &res, val);
+    return res;
+  }
+
+  template <typename T, enable_if_type_t<int64_t, T>* = nullptr>
+  T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
+    int64_t res;
+    JS_ToInt64(ctx, &res, val);
+    return res;
+  }
+
   template <typename T, enable_if_type_t<bool, T>* = nullptr>
   T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
     return static_cast<bool>(JS_ToBool(ctx, val));
@@ -44,11 +58,17 @@ namespace quickjsr {
     return result == "true" ? "TRUE" : result == "false" ? "FALSE" : result;
   }
 
+  template <typename T, enable_if_type_t<const char*, T>* = nullptr>
+  T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
+    return JSValue_to_Cpp<std::string>(ctx, val).c_str();
+  }
+
   template <typename T, typename std::enable_if<is_std_vector<T>::value>::type* = nullptr>
   T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
     T res;
     int64_t len;
     JS_GetLength(ctx, val, &len);
+    res.reserve(len);
     for (int64_t i = 0; i < len; i++) {
       JSValue elem = JS_GetPropertyInt64(ctx, val, i);
       res.push_back(JSValue_to_Cpp<value_type_t<T>>(ctx, elem));
