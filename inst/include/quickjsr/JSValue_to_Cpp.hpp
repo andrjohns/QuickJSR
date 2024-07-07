fc1a5e2f5b2b2edf9a2b91184aea99a0b6e7e653
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-07-07 14:17:25 +0300

    Add additional jsvalue conversion helpers

--- inst/include/quickjsr/JSValue_to_Cpp.hpp
+++ inst/include/quickjsr/JSValue_to_Cpp.hpp
@@ -31,17 +31,37 @@ namespace quickjsr {
     return res;
   }
 
+  template <typename T, enable_if_type_t<uint32_t, T>* = nullptr>
+  T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
+    uint32_t res;
+    JS_ToUint32(ctx, &res, val);
+    return res;
+  }
+
+  template <typename T, enable_if_type_t<int64_t, T>* = nullptr>
+  T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
+    int64_t res;
+    JS_ToInt64(ctx, &res, val);
+    return res;
+  }
+
   template <typename T, enable_if_type_t<bool, T>* = nullptr>
   T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
     return static_cast<bool>(JS_ToBool(ctx, val));
   }
 
-  template <typename T, enable_if_type_t<std::string, T>* = nullptr>
+  template <typename T, enable_if_type_t<const char*, T>* = nullptr>
   T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
     const char* res_str = JS_ToCString(ctx, val);
-    std::string result = res_str;
+    const char* result = (strcmp(res_str, "true") == 0) ? "TRUE"
+                          : ((strcmp(res_str, "false") == 0) ? "FALSE" : res_str);
     JS_FreeCString(ctx, res_str);
-    return result == "true" ? "TRUE" : result == "false" ? "FALSE" : result;
+    return result;
+  }
+
+  template <typename T, enable_if_type_t<std::string, T>* = nullptr>
+  T JSValue_to_Cpp(JSContext* ctx, JSValue val) {
+    return JSValue_to_Cpp<const char*>(ctx, val);
   }
 
   template <typename T, typename std::enable_if<is_std_vector<T>::value>::type* = nullptr>
@@ -49,6 +69,7 @@ namespace quickjsr {
     T res;
     int64_t len;
     JS_GetLength(ctx, val, &len);
+    res.reserve(len);
     for (int64_t i = 0; i < len; i++) {
       JSValue elem = JS_GetPropertyInt64(ctx, val, i);
       res.push_back(JSValue_to_Cpp<value_type_t<T>>(ctx, elem));
