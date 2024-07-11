d1ca06d76e5db20565f75d23a8ffddc232f570c4
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-07-11 22:01:31 +0300

    Tidy extern C usage (#69)
    * Tidy extern C usage
    
    * Spacing

--- src/init.cpp
+++ src/init.cpp
@@ -1,10 +1,7 @@
-#include <cpp11.hpp>
+#include <Rinternals.h>
 #include <R_ext/Visibility.h>
 
-#ifdef __cplusplus
 extern "C" {
-#endif
-
   SEXP qjs_context_(SEXP stack_size_);
   SEXP qjs_source_(SEXP ctx_ptr_, SEXP input_, SEXP is_file_);
   SEXP qjs_validate_(SEXP ctx_ptr_, SEXP code_string_);
@@ -16,7 +13,6 @@ extern "C" {
   SEXP from_json_(SEXP json_);
   SEXP qjs_version_();
 
-
   static const R_CallMethodDef CallEntries[] = {
     {"qjs_call_",     (DL_FUNC) &qjs_call_,     3},
     {"qjs_context_",  (DL_FUNC) &qjs_context_,  1},
@@ -25,18 +21,15 @@ extern "C" {
     {"qjs_validate_", (DL_FUNC) &qjs_validate_, 2},
     {"qjs_get_",      (DL_FUNC) &qjs_get_,      2},
     {"qjs_assign_",   (DL_FUNC) &qjs_assign_,   3},
-    {"to_json_", (DL_FUNC) &to_json_, 2},
-    {"from_json_", (DL_FUNC) &from_json_, 1},
-    {"qjs_version_", (DL_FUNC) &qjs_version_, 0},
+    {"to_json_",      (DL_FUNC) &to_json_,      2},
+    {"from_json_",    (DL_FUNC) &from_json_,    1},
+    {"qjs_version_",  (DL_FUNC) &qjs_version_,  0},
     {NULL, NULL, 0}
   };
 
-#ifdef __cplusplus
-}
-#endif
-
-extern "C" attribute_visible void R_init_QuickJSR(DllInfo* dll){
-  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-  R_useDynamicSymbols(dll, FALSE);
-  R_forceSymbols(dll, TRUE);
+  attribute_visible void R_init_QuickJSR(DllInfo* dll){
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+    R_forceSymbols(dll, TRUE);
+  }
 }
