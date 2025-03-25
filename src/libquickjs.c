0dae87a2b41ee66f107bb7956d352586ce8a56ac
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-03-25 22:32:30 +0800

    Remove static

--- src/libquickjs.c
+++ src/libquickjs.c
@@ -1,24 +1,3 @@
-#include <stdio.h>
-
-void Rf_error(const char *, ...);
-void Rprintf(const char *, ...);
-void Rf_abort(void) {
-  Rf_error("Aborted");
-}
-void Rf_exit(int status) {
-  Rf_error("Exit with status %d", status);
-}
-void Rf_putchar(int c) {
-  Rprintf("%c", c);
-}
-
-#define abort Rf_abort
-#define putchar Rf_putchar
-#define _putchar Rf_putchar
-#define exit Rf_exit
-#define _exit Rf_exit
-#define printf Rprintf
-
 #include "quickjs/cutils.c"
 #include "quickjs/libbf.c"
 #include "quickjs/libregexp.c"
