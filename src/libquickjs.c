b2df5a10c194c7b44c83bafd17a42c168ef0fa3b
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-03-25 20:16:09 +0800

    Masking

--- src/libquickjs.c
+++ src/libquickjs.c
@@ -1,3 +1,14 @@
+void Rf_error(const char *, ...);
+void Rf_abort(void) {
+  Rf_error("Aborted");
+}
+void Rf_exit(int status) {
+  Rf_error("Exit with status %d", status);
+}
+
+#define abort Rf_abort
+#define _exit Rf_exit
+
 #include "quickjs/cutils.c"
 #include "quickjs/libbf.c"
 #include "quickjs/libregexp.c"
