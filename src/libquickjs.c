6b61a95e7b38e909c7b4f965bcd1f23023c20d6a
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-03-25 20:35:16 +0800

    More wrappers

--- src/libquickjs.c
+++ src/libquickjs.c
@@ -1,12 +1,20 @@
+#include <stdio.h>
+
 void Rf_error(const char *, ...);
+void Rprintf(const char *, ...);
 void Rf_abort(void) {
   Rf_error("Aborted");
 }
 void Rf_exit(int status) {
   Rf_error("Exit with status %d", status);
 }
+void Rf_putchar(int c) {
+  Rprintf("%c", c);
+}
 
 #define abort Rf_abort
+#define putchar Rf_putchar
+#define exit Rf_exit
 #define _exit Rf_exit
 
 #include "quickjs/cutils.c"
