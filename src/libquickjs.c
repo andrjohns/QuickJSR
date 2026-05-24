e3fdebd11014dfd022ea71c4913cddb9bb209841
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2026-05-24 16:18:43 +0800

    Diverting io

--- src/libquickjs.c
+++ src/libquickjs.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+
+void Rprintf(const char *, ...);
+
+FILE* stdout_dummy;
+
+
+unsigned long fwrite_wrapper(const void * __ptr, size_t __size, size_t __nitems,
+                     FILE * __stream) {
+  if (__stream == stdout_dummy) {
+    Rprintf("%.*s", (int)(__size * __nitems), (const char*)__ptr);
+    return __nitems;
+  } else {
+    return fwrite(__ptr, __size, __nitems, __stream);
+  }
+}
+
+#define stdout stdout_dummy
+#define fwrite fwrite_wrapper
+
 #include "quickjs/dtoa.c"
 #include "quickjs/libregexp.c"
 #include "quickjs/libunicode.c"
